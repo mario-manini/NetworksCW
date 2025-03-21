bac1a64a6a1ec038076608020ce050836c64e2ce
mario-manini <146392313+mario-manini@users.noreply.github.com>
2025-03-21 20:30:28 +0000

    Add files via upload

---
+++ client/Client.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Client
+{
+	public static void main( String[] args )
+	{
+	}
+}
---
+++ server/Server.java
@@ -0,0 +1,11 @@
+import java.net.*;
+import java.io.*;
+import java.util.concurrent.*;
+
+public class Server
+{
+	public static void main( String[] args )
+	{
+	}
+}
+
